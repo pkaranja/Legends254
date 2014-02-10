67cf67b4621643836ac6a411e2db219f38b54cd4
pkaranja <pittskay@gmail.com>
2014-02-11 00:38:27 +0300

    Loosing weight
    Deleted unused files

--- src/com/ke/legends254/SplashActivity.java
+++ src/com/ke/legends254/SplashActivity.java
@@ -2,14 +2,25 @@ package com.ke.legends254;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.ke.legends254.util.*;
 
 public class SplashActivity extends Activity {
+	boolean isLoggedin = false;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		//Fetch here
-		//Pass to main intent
 		setContentView(R.layout.activity_splash);
+		
+		//Check if logged in
+		if ( isLoggedin().execute().equal(true) )
+		{
+			//TODO:Proceed to fetch data from mysql async task
+			//This guy is logged in
+			//Pass to main intent after fetching all data
+		}else{
+			//TODO:Redirect to login Activity	
+			//No data required
+		}
 	}
 }
---
+++ src/com/ke/legends254/util/common.java
@@ -0,0 +1,20 @@
+package com.ke.legends254.util;
+
+import android.os.AsyncTask;
+
+abstract class isLoggedin extends AsyncTask<Void, Boolean, Void> {
+	protected void onPreExecute() {
+        
+    }
+	
+     protected Void doInBackground(Void...params) {
+         
+    	 return null;
+     }
+
+     protected Boolean onPostExecute() {
+         
+    	 return true;
+     }
+ }
+ 
